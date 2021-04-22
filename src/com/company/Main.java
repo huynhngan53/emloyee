3d29d12d1ee1f6b1b100a365484fb010fd33335d
huynhngan53 <80590584+huynhngan53@users.noreply.github.com>
2021-04-22 19:16:57 +0700

    NHAP ID

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Map<String, String> mapEmployee = new TreeMap<>();
+        Scanner scanner = new Scanner(System.in);
+        String ID, Name;
+
+        mapEmployee.put("a1234","Steve Jobs");
+        mapEmployee.put("a1235","Scott McNealy");
+        mapEmployee.put("a1236","Jeff Bezos");
+        mapEmployee.put("a1237","Larry Ellison");
+        mapEmployee.put("a1238","Bill Gates");
+        System.out.println("Nhap ID");
+        ID= scanner.nextLine();
+        //mapEmployee.put("ID",);
+        /*Set<String> keySet=mapEmployee.keySet();
+
+        //Set<String> Value= (Set<String>) mapEmployee.values();
+        if (ID==mapEmployee.get(keySet)){
+           // String values = null;
+            System.out.println("ten la:"+mapEmployee.get(keySet));
+        }
+        else
+            System.out.println("NHAP SAI ID");
+
+        //System.out.println(mapEmployee.get("A1234"));
+       // System.out.println(mapEmployee.get("a1235"));
+        //System.out.println(mapEmployee.get("a1236"));
+        //System.out.println(mapEmployee.get("a1237"));
+        //System.out.println(mapEmployee.get("a1238"));
+        */
+
+
+
+
+    }
+
+}
